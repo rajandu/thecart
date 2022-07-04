d2e3c619fe3f54de0f8f33225cb5f19dfbbec26b
Rajan Kumar <smartrajanmuz@gmail.com>
2022-07-04 20:38:18 +0530

    created the splash screen

---
+++ app/src/main/java/in/thecart/SplashActivity.java
@@ -0,0 +1,20 @@
+package in.thecart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        SystemClock.sleep((2500));
+        startActivity(new Intent(SplashActivity.this,  RegisterActivity.class));
+        finish();
+    }
+}
