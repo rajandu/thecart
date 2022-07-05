927949a4a71ddc71dcd577e6fee02ee2b8398c28
Rajan Kumar <smartrajanmuz@gmail.com>
2022-07-05 14:05:58 +0530

    Added singup and login frame in register activity

---
+++ app/src/main/java/in/thecart/SignUpFragment.java
@@ -0,0 +1,57 @@
+package in.thecart;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SignUpFragment extends Fragment {
+
+    public SignUpFragment()
+    {
+
+    }
+
+    private TextView alreadyHaveAnAccount;
+    private FrameLayout parentFrameLayout;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+        alreadyHaveAnAccount = view.findViewById(R.id.alredyAccountSignUpView);
+        parentFrameLayout = getActivity().findViewById(R.id.register_frameLayout);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        alreadyHaveAnAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setFragment(new SignInFragment());
+            }
+        });
+    }
+
+    private void setFragment(Fragment fragment)
+    {
+        FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(parentFrameLayout.getId(),fragment);
+        fragmentTransaction.commit();
+    }
+}
